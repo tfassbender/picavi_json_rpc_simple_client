63edcda20e4bab27832ee07b7e71e7f99e765372
Tobias Faßbender <tobias-fassbender@gmx.de>
2019-06-29 18:18:28 +0200

    - added the server implementation PicaviJsonRpc_server as submodule (because the data models were needed) - added dependencies for JAX-RS and Jersey to the pom.xml - implemented sending and receiving data in the class SimpleRpcClient (request objects still need to be created)

--- src/main/java/com/picavi/json_rpc_simple_client/client/SimpleRpcClient.java
+++ src/main/java/com/picavi/json_rpc_simple_client/client/SimpleRpcClient.java
@@ -1,11 +1,25 @@
 package com.picavi.json_rpc_simple_client.client;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.picavi.json_rpc_server.model.JsonRpcRequest;
+import com.picavi.json_rpc_server.model.JsonRpcResponse;
 
 public class SimpleRpcClient {
 	
 	private static final Logger LOGGER = LogManager.getLogger(SimpleRpcClient.class);
+	
+	private static final String SERVER_URI = "http://localhost:9130";
+	private static final String RESOURCE_PATH_SYNC = "/";
+	private static final String RESOURCE_PATH_ASYNC = "/async";
 	
 	/**
 	 * Login the user with his password.
@@ -19,9 +33,29 @@ public class SimpleRpcClient {
 	 * @throws IllegalStateException
 	 *         An {@link IllegalStateException} is thrown if the login was not successful
 	 */
-	public void login(String username, String passwd) throws IllegalStateException {
+	public void login(String username, String password) throws IllegalStateException {
 		LOGGER.info("login: user: {}, password: <not shown here>", username);
-		//TODO login
+		//create a request for the login
+		JsonRpcRequest request = createLoginRequest(username, password);
+		
+		//send the request to the server via POST
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(SERVER_URI).path(RESOURCE_PATH_SYNC);
+		
+		LOGGER.info("Sending POST request to url: {}", SERVER_URI + RESOURCE_PATH_SYNC);
+		Response response = webTarget.request().accept(MediaType.APPLICATION_JSON).post(Entity.entity(request, MediaType.APPLICATION_JSON));
+		int responseCode = response.getStatus();
+		LOGGER.info("Server sent response code: " + responseCode);
+		
+		//check whether the response was OK or an error code
+		if (responseCode != Response.Status.OK.getStatusCode()) {
+			throw new IllegalStateException("HTTP error code: " + responseCode);
+		}
+	}
+	
+	private JsonRpcRequest createLoginRequest(String username, String password) {
+		//TODO create the login request
+		return null;
 	}
 	
 	/**
@@ -35,7 +69,27 @@ public class SimpleRpcClient {
 	 */
 	public void logout(String username) throws IllegalStateException {
 		LOGGER.info("logout: user: {}", username);
-		//TODO logout
+		//create a request for the logout
+		JsonRpcRequest request = createLogoutRequest(username);
+		
+		//send the request to the server via POST
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(SERVER_URI).path(RESOURCE_PATH_SYNC);
+		
+		LOGGER.info("Sending POST request to url: {}", SERVER_URI + RESOURCE_PATH_SYNC);
+		Response response = webTarget.request().accept(MediaType.APPLICATION_JSON).post(Entity.entity(request, MediaType.APPLICATION_JSON));
+		int responseCode = response.getStatus();
+		LOGGER.info("Server sent response code: " + responseCode);
+		
+		//check whether the response was OK or an error code
+		if (responseCode != Response.Status.OK.getStatusCode()) {
+			throw new IllegalStateException("HTTP error code: " + responseCode);
+		}
+	}
+	
+	private JsonRpcRequest createLogoutRequest(String username) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	/**
@@ -47,8 +101,41 @@ public class SimpleRpcClient {
 	 * @throws IllegalStateException
 	 *         An {@link IllegalStateException} is thrown when the request fails
 	 */
-	public void getPickList(String ident, boolean async) throws IllegalStateException {
+	public JsonRpcResponse getPickList(String ident, boolean async) throws IllegalStateException {
 		LOGGER.info("requesting picklist from server; identifier: {}, async: {}", ident, async);
-		//TODO get the picklist
+		//create a request to get the picklist from the server
+		JsonRpcRequest request = createGetPickListRequest(ident);
+		
+		//send the request to the server via POST
+		Client client = ClientBuilder.newClient();
+		WebTarget webTarget = client.target(SERVER_URI);
+		//choose the sync or async path
+		if (async) {
+			webTarget = webTarget.path(RESOURCE_PATH_ASYNC);
+		}
+		else {
+			webTarget = webTarget.path(RESOURCE_PATH_SYNC);
+		}
+		
+		LOGGER.info("Sending POST request to url: {}", SERVER_URI + RESOURCE_PATH_SYNC);
+		Response response = webTarget.request().accept(MediaType.APPLICATION_JSON).post(Entity.entity(request, MediaType.APPLICATION_JSON));
+		int responseCode = response.getStatus();
+		LOGGER.info("Server sent response code: " + responseCode);
+		
+		//check whether the response was OK or an error code and return the contained data (if it's OK)
+		if (responseCode != Response.Status.OK.getStatusCode()) {
+			throw new IllegalStateException("HTTP error code: " + responseCode);
+		}
+		else if (response.hasEntity()) {
+			return response.readEntity(JsonRpcResponse.class);
+		}
+		else {
+			throw new IllegalArgumentException("The response was expected to contain data, but it's empty");
+		}
+	}
+	
+	private JsonRpcRequest createGetPickListRequest(String ident) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
